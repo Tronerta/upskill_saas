7208ab5650b43aedf5d8b6eb220cb2d3648eed11
Tronerta <tronerta.web@gmail.com>
2018-07-15 12:47:05 +0000

    Added comments

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,24 +1,40 @@
 class ContactsController < ApplicationController
+
+  # GET request to /contact-us
+  # Show new contact form
   def new
     @contact = Contact.new
   end
 
-def create
-  @contact = Contact.new(contact_params)
-  if @contact.save
-     name = params[:contact][:name]
-     email = params[:contact][:email]
-     body = params[:contact][:comments]
-     ContactMailer.contact_email(name, email, body).deliver
-     flash[:success] = "Message sent."
-     redirect_to new_contact_path
-  else
-     flash[:danger] = @contact.errors.full_messages.join(", ")
-     redirect_to new_contact_path
+  # POST request to /contacts
+  def create
+    # Mass assignment of form fields into Contact object
+    @contact = Contact.new(contact_params)
+    # Save the Contact object to the database
+    if @contact.save
+      # Store form fields via paramaters, into variables
+       name = params[:contact][:name]
+       email = params[:contact][:email]
+       body = params[:contact][:comments]
+      # Plug variables into the contact mailer
+      # email method, and send email
+       ContactMailer.contact_email(name, email, body).deliver
+      # Store sucess messages into flash hash
+      # and redirect to the new action
+       flash[:success] = "Message sent."
+       redirect_to new_contact_path
+    else
+      # if Contact object doesnt save
+      # store errors in flash hash
+      # and redirect to the new action
+       flash[:danger] = @contact.errors.full_messages.join(", ")
+       redirect_to new_contact_path
+    end
   end
-end
 
   private
+    # To collect data from forms we need to use
+    # strong paramaters and whitelist the form fields
     def contact_params
       params.require(:contact).permit(:name, :email, :comments)
     end
