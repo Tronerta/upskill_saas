43c88578ae88c9ebd53a7d52f0dda02e9eadd58a
tronerta <tronerta.web@gmail.com>
2018-07-27 08:51:48 +0000

    Added Stripe respnse handler and error handling JS

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -9,6 +9,7 @@ $(document).on('turbolinks:load', function(){
   submitBtn.on('click', function(event){
     // prevent default form submit
     event.preventDefault();
+    submitBtn.val('Processing').prop('disabled', true);
 
     // collect credit catd fields
     let ccNum = $('#card_number').val(),
@@ -16,12 +17,48 @@ $(document).on('turbolinks:load', function(){
         expMonth = $('#card_month').val(),
         expYear = $('#card_year').val();
 
-    // send the card info to Stripe
-    Stripe.createToken({
-      number: ccNum,
-      cvc: cvcNum,
-      exp_month: expMonth,
-      exp_year: expYear
-    }, stripeResponseHandler);
+    // Use Stripe.js to validate
+    let error = false;
+
+    if(!Stripe.card.validateCardNumber(ccNum)) {
+      error = true;
+      alert('The credit card number appears to be invalid');
+    }
+
+    if(!Stripe.card.validateCVC(cvcNum)) {
+      error = true;
+      alert('The CVC number appears to be invalid');
+    }
+
+    if(!Stripe.card.validateExpiry(expMonth, expYear)) {
+      error = true;
+      alert('The expiration date appears to be invalid');
+    }
+
+    if (error) {
+      // if there is error dont send to Stripe
+      submitBtn.val('Sign Up').prop('disabled', false);
+    } else {
+      // send the card info to Stripe
+      Stripe.createToken({
+        number: ccNum,
+        cvc: cvcNum,
+        exp_month: expMonth,
+        exp_year: expYear
+      }, stripeResponseHandler);
+    }
+
+    return false;
   });
+
+  // Stripe response with a token
+  function stripeResponseHandler (status, response){
+    let token = response.id;
+
+    // Inject card token in hidden field
+    theForm.append( $('<input type="hidden" name="user[stripe_card_token]">').val(token) );
+
+    // submit form to Rails app
+    theForm.get(0).submit();
+  }
 })
