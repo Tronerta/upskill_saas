82ac376c2231b130ef6dfd52bc89b92baa68ce54
tronerta <tronerta.web@gmail.com>
2019-01-09 14:01:20 +0000

    Added helper for job title icon

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,9 @@
 //= require turbolinks
 //= require_tree .
 //= require bootstrap-sprockets
+
+/* global $, FontAwesome */
+
+$(document).on('turbolinks:load', function(){
+  FontAwesome.dom.i2svg();
+})
