30385fc610211bb3a17d30e4d2a2bd461842f809
tronerta <tronerta.web@gmail.com>
2019-06-08 10:31:56 +0000

    Stripe secured

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,7 +1,2 @@
-# Stripe.api_key = ENV["stripe_api_key"]
-# STRIPE_PUBLIC_KEY = ENV["stripe_publishable_key"]
-
-Rails.configuration.stripe = {
-  :publishable_key => 'pk_test_yZFwPWGhc1fbMU8q8ATYQZHH',
-  :secret_key      => 'sk_test_LfFe10K6GXIWajZUiObffSEY'
-}
+Stripe.api_key = ENV["stripe_api_key"]
+STRIPE_PUBLIC_KEY = ENV["stripe_publishable_key"]
